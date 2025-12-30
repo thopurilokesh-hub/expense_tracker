fe7432b63c5fa12a83ca6356216e0e0e73ba35ed
Lokesh Thopuri <thopurilokesh@gmail.com>
2025-12-30 14:34:58 +0530

    update

--- expense_analysis.py
+++ expense_analysis.py
@@ -9,8 +9,12 @@ df["Date"]=pd.to_datetime(df["Date"])
 #Monthly expense analysis
 df["Month"]=df["Date"].dt.month
 monthly_expenses=df.groupby("Month")["Amount"].sum()
-print(f"Monthly expenses is :{monthly_expenses}")
+#print(f"Monthly expenses is :{monthly_expenses}")
 #category expense analysis
 category_expense=df.groupby("Category")["Amount"].sum()
-print(f"Category expenses :{category_expense}")
+#print(f"Category expenses :{category_expense}")
+#Highest Spending Category
+highest_category=category_expense.idxmax()
+highest_amount=category_expense.max()
+print(f"Highest category is: {highest_category} and highest amount is:{highest_amount} ")
 
